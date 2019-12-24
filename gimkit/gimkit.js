81e6a9c8d51c95454f39b4e1dd21e32f88c5a54d
UndercoverGoose <53491740+UndercoverGoose@users.noreply.github.com>
2019-12-24 15:54:59 -0500

    Update gimkit.js

--- gimkit/gimkit.js
+++ gimkit/gimkit.js
@@ -60,7 +60,6 @@
 
   let b = cre(d,btnattr.class,"position: fixed; z-index: 1000000; left: 5px; bottom: 5px; width: 100px; height: 40px;","Menu");
   b.onclick=function(){if(f[0]===0){gcn(cN,1).style.display="block",f[0]=1,gcn(cN,0).innerHTML="X",gcn(cN,0).style.background="#ff0000";}else{gcn(cN,1).style.display="none",f[0]=0,gcn(cN,0).innerHTML="Menu",gcn(cN,0).style.background="";}};
-  b.innerHTML = localStorage.getItem('blueboat-id');
   app(b);
 
   // Area for cheat buttons
