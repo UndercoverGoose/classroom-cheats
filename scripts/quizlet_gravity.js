823c92062bd8928f88ac5436ec41a8c7a63cd472
UndercoverGoose <53491740+UndercoverGoose@users.noreply.github.com>
2022-02-16 00:07:40 -0500

    Create quizlet_gravity.js

---
+++ scripts/quizlet_gravity.js
@@ -0,0 +1,31 @@
+(function () {
+  if (!window.gooseGravity) {
+    window.gooseGravity = true;
+    const matches = {};
+    Quizlet.gravityModeData.terms.forEach((term) => {
+      matches[term.word] = term.definition;
+    });
+    const reversed = {};
+    for (const term in matches) {
+      reversed[matches[term]] = term;
+    }
+    setInterval(function () {
+      const asteroids = Array.from(
+        document.getElementsByClassName("TermText notranslate lang-sv")
+      );
+      if (asteroids.length === 0) return;
+
+      asteroids.forEach((asteroid) => {
+        const term = asteroid.textContent;
+        if (asteroid.gooseAnswered) return;
+        if (term in matches) {
+          asteroid.textContent = matches[term];
+          asteroid.gooseAnswered = true;
+        } else if (term in reversed) {
+          asteroid.textContent = reversed[term];
+          asteroid.gooseAnswered = true;
+        }
+      });
+    }, 100);
+  }
+})();
